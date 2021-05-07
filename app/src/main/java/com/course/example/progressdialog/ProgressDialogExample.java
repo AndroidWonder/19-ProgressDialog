59c2c4088dc412535c1338e77465ad0c00541fd8
Android Wonder <jim.pepe@verizon.net>
2021-05-07 14:40:10 -0400

    Rotating Progress Bar

--- app/src/main/java/com/course/example/progressdialog/ProgressDialogExample.java
+++ app/src/main/java/com/course/example/progressdialog/ProgressDialogExample.java
@@ -1,10 +1,12 @@
 package com.course.example.progressdialog;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 import android.os.Message;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.view.KeyEvent;
 import android.widget.TextView;
 
@@ -12,22 +14,29 @@ public class ProgressDialogExample extends Activity implements Runnable {
 
 	private String pi_string;
 	private TextView tv;
-	private ProgressDialog pd;
+	private ProgressBar progress;
+
+	private Handler handler = new Handler(Looper.getMainLooper()) {
+		@Override
+		public void handleMessage(Message msg) {
+
+			tv.setText(pi_string);
+			progress.setVisibility(View.INVISIBLE);
+		}
+	};
 
 	@Override
 	public void onCreate(Bundle icicle) {
 		super.onCreate(icicle);
 		setContentView(R.layout.main);
 
+		progress = (ProgressBar) findViewById(R.id.progress);
 		tv = (TextView) findViewById(R.id.TextView01);
 		tv.setText("Press any key to start calculation" + "\n");
 	}
 
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
-
-		pd = ProgressDialog.show(this, "Working..", "Calculating Pi", true,
-				false);
 
 		Thread thread = new Thread(this);
 		thread.start();
@@ -36,17 +45,7 @@ public class ProgressDialogExample extends Activity implements Runnable {
 	}
 
 	public void run() {
-		pi_string = Pi.computePi(800).toString();
+		pi_string = Pi.computePi(1500).toString();
 		handler.sendEmptyMessage(0);
 	}
-
-	private Handler handler = new Handler() {
-		@Override
-		public void handleMessage(Message msg) {
-			pd.dismiss();
-			tv.setText(pi_string);
-
-		}
-	};
-
 }
